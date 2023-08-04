39b9061b81f577446f1804cf65c7c3e1eb3622fb
Kirill <dorc99@mail.ru>
2023-08-04 21:59:01 +0700

    дубль 6, продолжаю попытки пройти автотесты яндекса. p.s. уже прошел, добавил валидацию, поглядим как отреагируют тесты

--- models/card.js
+++ models/card.js
@@ -10,6 +10,10 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: [true, 'Поле link должно быть заполнено'],
+    validate: {
+      validator: (v) => /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&=]*)$/.test(v),
+      message: 'Поле должно содержать url',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
